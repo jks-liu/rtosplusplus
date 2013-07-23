7d7a8c4f2627d0fb5decefb6088b432bdcac8b97
jks liu <chinatianma@gmail.com>
2013-07-23 21:31:15 +0800

    xxx

--- rtosplusplus.cc
+++ rtosplusplus.cc
@@ -1,15 +1,23 @@
 #include "rtosplusplus.h"
-#include "trosplusplus-config.h"
 #include <avr/io.h>
 #include <avr/Interrupt.h>
 #include <inttypes.h>
 
-static uint8_t register ready_thread asm("r2");
-static uint8_t register runing
+static uint8_t register g_ready_thread asm("r2");
+static uint8_t register g_runing_thread asm("r3");
 
-static 
+static void(*g_hook)(RtosPlusPlus *);
 
-RtosPlusPlus::RtosPlusPlus(void) {
+RtosPlusPlus::RtosPlusPlus(void(*hook)(RtosPlusPlus *)):
+  threads_num_(0)
+{
+  g_hook = hook;
+}
+
+RtosPlusPlus::creat(RtosPlusPlus::TCB const &thread)
+{
+  memcpy(&tcbs_[threads_num_++], &thread, sizeof(thread));
+}
 
 
 
--- rtosplusplus.h
+++ rtosplusplus.h
@@ -1,17 +1,21 @@
 #ifndef RTOSPLUSPLUS_H_
 #define RTOSPLUSPLUS_H_
 
+#include "rtosplusplus-config.h"
+
 class RtosPlusPlus {
 public:
-  struct Thread {
+  struct TCB { // Thread Control Block
     /* The stack grow down, stack_top points invalid data. */
     unsigned int stack_top;
+    void *(*start_routine)(void *);
+    void *arg
   };
-  RtosPlusPlus(void);
-  int create(Thread *t,
-             const int /* must be 0*/,
-             void *(*start_routine)(void *),
-             void *arg);
+  RtosPlusPlus(void(*hook)(RtosPlusPlus *));
+  int create(TCB const &thread);
+private:
+  uint8_t threads_num_;
+  TCB tcbs_[THREADS_NUM];
 };
 
 #endif /* RTOSPLUSPLUS_H_ */
